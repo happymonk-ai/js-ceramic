2c2d182774138a30bbde5a36b6e752d3484db139
iamhappymonk <bhavish@happymonk.co>
2021-07-01 19:14:38 +0530

    build push

--- packages/key-did-resolver/src/index.ts
+++ packages/key-did-resolver/src/index.ts
@@ -19,34 +19,35 @@ const prefixToDriverMap: any = {
   0xED: ed25519,
 }
 
-export default {
-  getResolver: (): ResolverRegistry => ({
-    'key': async (did: string, parsed: ParsedDID, r: Resolver, options: DIDResolutionOptions): Promise<DIDResolutionResult> => {
-      const contentType = options.accept || DID_JSON
-      const response: DIDResolutionResult = {
-        didResolutionMetadata: { contentType },
-        didDocument: null,
-        didDocumentMetadata: {}
-      }
-      try {
-        const multicodecPubKey: any = multibase.decode(parsed.id)
-        const keyType = varint.decode(multicodecPubKey)
-        const pubKeyBytes = multicodecPubKey.slice(varint.decode.bytes)
-        const doc = await prefixToDriverMap[keyType].keyToDidDoc(pubKeyBytes, parsed.id)
-        if (contentType === DID_LD_JSON) {
-          doc['@context'] = 'https://w3id.org/did/v1',
-          response.didDocument = doc
-        } else if (contentType === DID_JSON) {
-          response.didDocument = doc
-        } else {
-          delete response.didResolutionMetadata.contentType
-          response.didResolutionMetadata.error = 'representationNotSupported'
-        }
-      } catch (e) {
-        response.didResolutionMetadata.error = 'invalidDid'
-        response.didResolutionMetadata.message = e.toString()
-      }
-      return response
+
+const getResolver = (): ResolverRegistry => ({
+  'key': async (did: string, parsed: ParsedDID, r: Resolver, options: DIDResolutionOptions): Promise<DIDResolutionResult> => {
+    const contentType = options.accept || DID_JSON
+    const response: DIDResolutionResult = {
+      didResolutionMetadata: { contentType },
+      didDocument: null,
+      didDocumentMetadata: {}
     }
-  })
-}
+    try {
+      const multicodecPubKey: any = multibase.decode(parsed.id)
+      const keyType = varint.decode(multicodecPubKey)
+      const pubKeyBytes = multicodecPubKey.slice(varint.decode.bytes)
+      const doc = await prefixToDriverMap[keyType].keyToDidDoc(pubKeyBytes, parsed.id)
+      if (contentType === DID_LD_JSON) {
+        doc['@context'] = 'https://w3id.org/did/v1',
+          response.didDocument = doc
+      } else if (contentType === DID_JSON) {
+        response.didDocument = doc
+      } else {
+        delete response.didResolutionMetadata.contentType
+        response.didResolutionMetadata.error = 'representationNotSupported'
+      }
+    } catch (e) {
+      response.didResolutionMetadata.error = 'invalidDid'
+      response.didResolutionMetadata.message = e.toString()
+    }
+    return response
+  }
+})
+
+export default getResolver();
